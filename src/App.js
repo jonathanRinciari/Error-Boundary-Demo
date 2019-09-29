ff99c6365b11fff79a04a0c976596b9184997921
jon rinciari <rinciarijoc@gmail.com>
2019-09-28 22:23:17 -0400

    added app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import KeyPad from "./KeyPad/KeyPad";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ErrorBoundary>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "275px",
+            justifyContent: "center",
+            marginLeft: "2rem"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <h2>
+              Matching either keycode results in <u><i>both keypads from being usable
+              due to an error.</i></u>
+            </h2>
+            <p style={{minHeight: '90px'}}>This lets us wrap a large section of our app and catch errors</p>
+          </div>
+          <KeyPad matcher="↓→↓←↓" />
+          <KeyPad matcher="↓→↓↓←" />
+        </div>
+      </ErrorBoundary>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "275px",
+          justifyContent: "center",
+          marginLeft: "2rem"
+        }}
+      >
+        <div>
+          <h2>
+            Matcher either of the keycodes will result in the <u><i>matching keypad to
+            display the error.</i></u>
+          </h2>
+          <p>
+            We can use error boundaries to capture errors around smaller
+            sections of our app. This prevents the whole app from being unusable
+            due to the error.
+          </p>
+        </div>
+        <ErrorBoundary>
+          <KeyPad matcher="↓→↑→↓" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <KeyPad matcher="↑→↑↓←" />
+        </ErrorBoundary>
+      </div>
     </div>
   );
 }
